489848918872f907670e5ff9b932f726a508d8f9
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-04-22 13:28:25 -0700

    Platapus

---
+++ src/Platapus.java
@@ -0,0 +1,9 @@
+public class Platapus {
+    private String name;
+    public Platapus(String newName) {
+    	name = newName;
+    }
+    void sayHi(){
+        System.out.println("The platypus " + name + " is smarter than your average platypus.");
+    }
+}
---
+++ src/PlatapusRunner.java
@@ -0,0 +1,21 @@
+
+public class PlatapusRunner {
+public static void main(String[] args) {
+	Platapus p1 = new Platapus("Billy");
+	p1.sayHi();
+	Platapus p2 = new Platapus("Boby");
+	p2.sayHi();
+	Platapus p3 = new Platapus("Fred");
+	p3.sayHi();
+	Platapus p4 = new Platapus("Bob");
+	p4.sayHi();
+	Platapus p5 = new Platapus("Happy");
+	p5.sayHi();
+	Platapus p6 = new Platapus("Yay");
+	p6.sayHi();
+	Platapus p7 = new Platapus("Hater");
+	p7.sayHi();
+	Platapus p8 = new Platapus("every");
+	p8.sayHi();
+}
+}
